c1abbfa1e7efdf15d2b40d9340949f5e9f5623e0
Devendra <dsingh@ebizneeds.com>
2018-03-22 13:57:12 +0530

    worker js update

--- addtohomescreen/worker.js
+++ addtohomescreen/worker.js
@@ -1,98 +1,27 @@
-var CACHE_NAME = 'my-site-cache-v11';
-var urlsToCache = [
-  '/'
-];
-
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('ds-pwa-a2hs').then(function(cache) {
+     return cache.addAll([
+       '/pwa/addtohomescreen/',
+       '/pwa/addtohomescreen/index.html',
+       '/pwa/addtohomescreen/service-worker.js',
+       '/pwa/addtohomescreen/icons/favicon.ico',
+       '/pwa/addtohomescreen/icons/icon-36x36.png',
+       '/pwa/addtohomescreen/icons/icon-48x48.png',
+       '/pwa/addtohomescreen/icons/icon-72x72.png'
+       '/pwa/addtohomescreen/icons/icon-96x96.png'
+       '/pwa/addtohomescreen/icons/icon-144x144.png'
+       '/pwa/addtohomescreen/icons/icon-192x192.png'
+     ]);
+   })
+ );
 });
 
-/* self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
-        var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-    );
-}); */
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-    .then(function(response) {
-      return response || fetchAndCache(event.request);
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
-
-function fetchAndCache(url) {
-  return fetch(url)
-  .then(function(response) {
-    // Check if we received a valid response
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    return caches.open(CACHE_NAME)
-    .then(function(cache) {
-      cache.put(url, response.clone());
-      return response;
-    });
-  })
-  .catch(function(error) {
-    console.log('Request failed:', error);
-    // You could return a custom offline 404 page here
-  });
-}
-/* self.addEventListener('activate', function(event) {
-
-  var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
-
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-}); */
